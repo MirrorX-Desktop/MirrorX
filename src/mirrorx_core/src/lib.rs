950e781dd5ccdd1eeec276d60a572738aa0f4aff
chenbaiyu <cby40899570@gmail.com>
2022-03-11 01:51:42 +0800

    commit

--- src/mirrorx_core/src/lib.rs
+++ src/mirrorx_core/src/lib.rs
@@ -1,4 +1,4 @@
-pub mod api;
+mod api;
 mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 mod duplicator;
 mod ffi_log;
