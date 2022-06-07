e2b7a6dd75bd1150fd3650c5939c51f3ba9f632c
chenbaiyu <cby40899570@gmail.com>
2022-06-08 01:21:07 +0800

    commit

--- src/app/ios/Flutter/flutter_export_environment.sh
+++ src/app/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/chenbaiyu/workspace/mirrorx/src/app"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=F:\MirrorX\src\app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
