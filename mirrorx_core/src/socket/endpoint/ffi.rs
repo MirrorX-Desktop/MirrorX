1077e7ba6cf020a6f62d4f942359ecca6820634c
chenbaiyu <cby40899570@gmail.com>
2022-06-29 05:15:14 +0800

    commit

--- mirrorx_core/src/socket/endpoint/ffi.rs
+++ mirrorx_core/src/socket/endpoint/ffi.rs
@@ -19,7 +19,7 @@ pub const unsafe fn create_callback_fn(
     callback_ptr: i64,
 ) -> unsafe extern "C" fn(*mut c_void, *const u8, usize, usize) {
     std::mem::transmute::<
-        *mut c_void,
+        *const c_void,
         unsafe extern "C" fn(
             video_texture_ptr: *mut c_void,
             frame_buffer: *const u8,
