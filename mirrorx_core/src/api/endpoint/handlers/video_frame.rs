395c3ea08ead39becc585ad42df88034cd01e8ed
chenbaiyu <cby40899570@gmail.com>
2022-09-12 20:06:20 +0800

    commit

--- mirrorx_core/src/api/endpoint/handlers/video_frame.rs
+++ mirrorx_core/src/api/endpoint/handlers/video_frame.rs
@@ -6,3 +6,33 @@ pub async fn handle_video_frame(
     video_frame: EndPointVideoFrame,
 ) {
 }
+
+use std::os::raw::c_void;
+
+#[cfg(target_os = "macos")]
+use crate::ffi::os::macos::core_video::CVPixelBufferRef;
+
+#[cfg(target_os = "macos")]
+pub const unsafe fn create_callback_fn(
+    callback_ptr: i64,
+) -> unsafe extern "C" fn(*mut c_void, CVPixelBufferRef) {
+    std::mem::transmute::<
+        *const c_void,
+        unsafe extern "C" fn(video_texture_ptr: *mut c_void, pixel_buffer: CVPixelBufferRef),
+    >(callback_ptr as *const c_void)
+}
+
+#[cfg(target_os = "windows")]
+pub const unsafe fn create_callback_fn(
+    callback_ptr: i64,
+) -> unsafe extern "C" fn(*mut c_void, *const u8, usize, usize) {
+    std::mem::transmute::<
+        *const c_void,
+        unsafe extern "C" fn(
+            video_texture_ptr: *mut c_void,
+            frame_buffer: *const u8,
+            frame_width: usize,
+            frame_height: usize,
+        ),
+    >(callback_ptr as *const c_void)
+}
