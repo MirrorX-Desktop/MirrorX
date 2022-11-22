02d6d547520a7430ba13cc493dd73321be4e0a4f
chenbaiyu <cby40899570@gmail.com>
2022-11-22 18:10:38 +0800

    disable context menu according to mode env

--- mirrorx_frontend/src/routes/+layout.svelte
+++ mirrorx_frontend/src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import { loadAllLocales } from '../i18n/i18n-util.sync';
 
 	loadAllLocales();
+
+	if (import.meta.env.PROD) {
+		document.addEventListener('contextmenu', (event) => event.preventDefault());
+	}
 </script>
 
 <slot />
