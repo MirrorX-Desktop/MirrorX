2bb3f77a31cd07ad8d73261500056f5717ef7d38
chenbaiyu <cby40899570@gmail.com>
2023-01-06 16:41:10 +0800

    refactor(windows): enhance get icon

--- mirrorx_core/src/component/fs/mod.rs
+++ mirrorx_core/src/component/fs/mod.rs
@@ -1,6 +1,9 @@
 #[cfg(target_os = "macos")]
 mod macos;
 
+#[cfg(target_os = "windows")]
+mod windows;
+
 use crate::error::CoreResult;
 use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
@@ -22,45 +25,20 @@ pub struct Entry {
     pub icon: Option<Vec<u8>>,
 }
 
-#[cfg(not(target_os = "windows"))]
 pub fn read_root_directory() -> CoreResult<Directory> {
-    read_directory("/")
+    #[cfg(not(target_os = "windows"))]
+    return read_directory("/");
+
+    #[cfg(target_os = "windows")]
+    return self::windows::read_root_directory();
 }
 
-#[cfg(target_os = "windows")]
-pub fn read_root_directory() -> CoreResult<Directory> {
-    use std::str::FromStr;
-    use windows::Win32::Storage::FileSystem::GetLogicalDrives;
+fn read_icon(path: &Path) -> CoreResult<Vec<u8>> {
+    #[cfg(not(target_os = "windows"))]
+    return self::macos::NSWorkspace::sharedWorkspace()?.iconForFile(path);
 
-    let mut entries = Vec::new();
-
-    unsafe {
-        let mask = GetLogicalDrives();
-        for i in 0..u32::BITS {
-            if (mask >> i) & 1 == 0 {
-                continue;
-            }
-
-            let disk = [b'A' + i as u8, b':', b'\\'];
-            let disk_str = std::str::from_utf8_unchecked(&disk);
-            let path = PathBuf::from_str(disk_str)?;
-
-            let icon = read_icon(&path).map_or(None, |v| Some(v));
-
-            entries.push(Entry {
-                is_dir: true,
-                path,
-                modified_time: 0,
-                size: 0,
-                icon,
-            });
-        }
-    }
-
-    Ok(Directory {
-        path: PathBuf::from(r"\"),
-        entries,
-    })
+    #[cfg(target_os = "windows")]
+    return self::windows::read_icon(path);
 }
 
 pub fn read_directory<P>(path: P) -> CoreResult<Directory>
@@ -119,158 +97,4 @@ where
         path: path.into(),
         entries,
     })
-}
-
-#[cfg(not(target_os = "windows"))]
-fn read_icon(path: &Path) -> CoreResult<Vec<u8>> {
-    self::macos::NSWorkspace::sharedWorkspace()?.iconForFile(path)
-}
-
-#[cfg(target_os = "windows")]
-fn read_icon(path: &Path) -> CoreResult<Vec<u8>> {
-    use crate::core_error;
-    use image::ColorType;
-    use scopeguard::defer;
-    use std::{io::Cursor, os::raw::c_void};
-    use windows::{
-        core::PCWSTR,
-        Win32::{
-            Graphics::Gdi::{
-                DeleteObject, GetDC, GetDIBits, GetObjectW, ReleaseDC, BITMAP, BITMAPINFO,
-                BITMAPINFOHEADER, BI_RGB, DIB_RGB_COLORS,
-            },
-            Storage::FileSystem::FILE_ATTRIBUTE_NORMAL,
-            UI::{
-                Shell::{SHGetFileInfoW, SHFILEINFOW, SHGFI_ICON, SHGFI_LARGEICON},
-                WindowsAndMessaging::{DestroyIcon, GetIconInfo, ICONINFO},
-            },
-        },
-    };
-
-    unsafe {
-        let path = widestring::WideCString::from_os_str(path)
-            .map_err(|err| core_error!("convert wide string failed ({})", err))?;
-
-        let mut psfi: SHFILEINFOW = std::mem::zeroed();
-
-        let ret = SHGetFileInfoW(
-            PCWSTR::from_raw(path.as_ptr()),
-            FILE_ATTRIBUTE_NORMAL,
-            Some(&mut psfi),
-            std::mem::size_of::<SHFILEINFOW>() as _,
-            SHGFI_ICON | SHGFI_LARGEICON,
-        );
-
-        if ret == 0 {
-            return Err(core_error!("SHGetFileInfoW failed"));
-        }
-
-        defer! {
-            let _ = DestroyIcon(psfi.hIcon);
-        }
-
-        let mut icon_info: ICONINFO = std::mem::zeroed();
-        if !GetIconInfo(psfi.hIcon, &mut icon_info).as_bool() {
-            return Err(core_error!("GetIconInfo failed"));
-        }
-
-        let dc = GetDC(None);
-        let mut bitmap: BITMAP = std::mem::zeroed();
-        if GetObjectW(
-            icon_info.hbmColor,
-            std::mem::size_of::<BITMAP>() as _,
-            Some(&mut bitmap as *mut _ as *mut c_void),
-        ) == 0
-        {
-            return Err(core_error!("GetObjectW failed"));
-        }
-
-        let mut bitmap_info: BITMAPINFO = std::mem::zeroed();
-        bitmap_info.bmiHeader.biSize = std::mem::size_of::<BITMAPINFOHEADER>() as _;
-        bitmap_info.bmiHeader.biWidth = bitmap.bmWidth;
-        bitmap_info.bmiHeader.biHeight = -bitmap.bmHeight;
-        bitmap_info.bmiHeader.biPlanes = 1;
-        bitmap_info.bmiHeader.biBitCount = 32;
-        bitmap_info.bmiHeader.biCompression = BI_RGB;
-
-        let nbits = (bitmap.bmWidth * bitmap.bmHeight) as usize;
-        let mut color_bits = Vec::<u32>::with_capacity(nbits);
-        if GetDIBits(
-            dc,
-            icon_info.hbmColor,
-            0,
-            bitmap.bmHeight as _,
-            Some(color_bits.as_mut_ptr() as *mut c_void),
-            &mut bitmap_info,
-            DIB_RGB_COLORS,
-        ) == 0
-        {
-            return Err(core_error!("GetDIBits failed"));
-        }
-
-        color_bits.set_len(nbits);
-
-        let mut has_alpha = false;
-        for i in 0..nbits {
-            if color_bits[i] & 0xFF000000 != 0 {
-                has_alpha = true;
-                break;
-            }
-        }
-
-        if !has_alpha {
-            let mut mask_bits = Vec::<u32>::with_capacity(nbits);
-            if GetDIBits(
-                dc,
-                icon_info.hbmMask,
-                0,
-                bitmap.bmHeight as _,
-                Some(mask_bits.as_mut_ptr() as *mut c_void),
-                &mut bitmap_info,
-                DIB_RGB_COLORS,
-            ) == 0
-            {
-                return Err(core_error!("GetDIBits failed"));
-            }
-            mask_bits.set_len(nbits);
-
-            for i in 0..nbits {
-                if mask_bits[i] == 0 {
-                    color_bits[i] |= 0xFF000000;
-                }
-            }
-        }
-
-        ReleaseDC(None, dc);
-        DeleteObject(icon_info.hbmColor);
-        DeleteObject(icon_info.hbmMask);
-
-        let bmp_bytes =
-            std::slice::from_raw_parts_mut(color_bits.as_mut_ptr() as *mut u8, nbits * 4);
-
-        // swap BGRA to RGBA
-        for chunk in bmp_bytes.chunks_mut(4).into_iter() {
-            chunk[0] = chunk[0] ^ chunk[2];
-            chunk[2] = chunk[0] ^ chunk[2];
-            chunk[0] = chunk[0] ^ chunk[2];
-        }
-
-        let mut png_bytes: Vec<u8> = Vec::with_capacity(nbits * 4);
-
-        if let Err(err) = image::write_buffer_with_format(
-            &mut Cursor::new(&mut png_bytes),
-            bmp_bytes,
-            bitmap.bmWidth as u32,
-            bitmap.bmHeight as u32,
-            ColorType::Rgba8,
-            image::ImageOutputFormat::Png,
-        ) {
-            return Err(core_error!(
-                "write desktop screenshot image buffer failed ({})",
-                err
-            ));
-        }
-
-        Ok(png_bytes)
-    }
 }
---
+++ mirrorx_core/src/component/fs/windows.rs
@@ -0,0 +1,158 @@
+use super::{Directory, Entry};
+use crate::{core_error, error::CoreResult, HRESULT};
+use image::ColorType;
+use scopeguard::defer;
+use std::{
+    io::Cursor,
+    os::raw::c_void,
+    path::{Path, PathBuf},
+    str::FromStr,
+};
+use windows::{
+    core::PCWSTR,
+    Win32::{
+        Foundation::SIZE,
+        Graphics::Gdi::{
+            DeleteObject, GetDC, GetDIBits, GetObjectW, ReleaseDC, BITMAP, BITMAPINFO,
+            BITMAPINFOHEADER, BI_RGB, DIB_RGB_COLORS,
+        },
+        Storage::FileSystem::GetLogicalDrives,
+        UI::Shell::{IShellItemImageFactory, SHCreateItemFromParsingName, SIIGBF_ICONONLY},
+    },
+};
+
+static TRY_ICON_SIZES: [(i32, i32); 6] = [
+    (64, 64),
+    (128, 128),
+    (48, 48),
+    (32, 32),
+    (256, 256),
+    (16, 16),
+];
+
+pub fn read_root_directory() -> CoreResult<Directory> {
+    let mut entries = Vec::new();
+
+    unsafe {
+        let mask = GetLogicalDrives();
+        for i in 0..u32::BITS {
+            if (mask >> i) & 1 == 0 {
+                continue;
+            }
+
+            let disk = [b'A' + i as u8, b':', b'\\'];
+            let disk_str = std::str::from_utf8_unchecked(&disk);
+            let path = PathBuf::from_str(disk_str)?;
+
+            let icon = read_icon(&path).map_or(None, |v| Some(v));
+
+            entries.push(Entry {
+                is_dir: true,
+                path,
+                modified_time: 0,
+                size: 0,
+                icon,
+            });
+        }
+    }
+
+    Ok(Directory {
+        path: PathBuf::from(r"\"),
+        entries,
+    })
+}
+
+pub fn read_icon(path: &Path) -> CoreResult<Vec<u8>> {
+    unsafe {
+        let path = widestring::WideCString::from_os_str(path)
+            .map_err(|err| core_error!("convert wide string failed ({})", err))?;
+
+        let factory: IShellItemImageFactory = HRESULT!(SHCreateItemFromParsingName(
+            PCWSTR::from_raw(path.as_ptr()),
+            None
+        ));
+
+        let mut hbitmap = None;
+        for (cx, cy) in TRY_ICON_SIZES {
+            match factory.GetImage(SIZE { cx, cy }, SIIGBF_ICONONLY) {
+                Ok(v) => {
+                    hbitmap = Some(v);
+                    break;
+                }
+                Err(_) => continue,
+            }
+        }
+
+        let hbitmap = hbitmap.ok_or_else(|| core_error!("all sizes icon is empty"))?;
+
+        defer! {
+           let  _ = DeleteObject(hbitmap);
+        }
+
+        let dc = GetDC(None);
+        let mut bitmap: BITMAP = std::mem::zeroed();
+        if GetObjectW(
+            hbitmap,
+            std::mem::size_of::<BITMAP>() as _,
+            Some(&mut bitmap as *mut _ as *mut c_void),
+        ) == 0
+        {
+            return Err(core_error!("GetObjectW failed"));
+        }
+
+        let mut bitmap_info: BITMAPINFO = std::mem::zeroed();
+        bitmap_info.bmiHeader.biSize = std::mem::size_of::<BITMAPINFOHEADER>() as _;
+        bitmap_info.bmiHeader.biWidth = bitmap.bmWidth;
+        bitmap_info.bmiHeader.biHeight = -bitmap.bmHeight;
+        bitmap_info.bmiHeader.biPlanes = 1;
+        bitmap_info.bmiHeader.biBitCount = 32;
+        bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+        let nbits = (bitmap.bmWidth * bitmap.bmHeight) as usize;
+        let mut color_bits = Vec::<u32>::with_capacity(nbits);
+        if GetDIBits(
+            dc,
+            hbitmap,
+            0,
+            bitmap.bmHeight as _,
+            Some(color_bits.as_mut_ptr() as *mut c_void),
+            &mut bitmap_info,
+            DIB_RGB_COLORS,
+        ) == 0
+        {
+            return Err(core_error!("GetDIBits failed"));
+        }
+
+        color_bits.set_len(nbits);
+
+        ReleaseDC(None, dc);
+
+        let bmp_bytes =
+            std::slice::from_raw_parts_mut(color_bits.as_mut_ptr() as *mut u8, nbits * 4);
+
+        // swap BGRA to RGBA
+        for chunk in bmp_bytes.chunks_mut(4).into_iter() {
+            chunk[0] = chunk[0] ^ chunk[2];
+            chunk[2] = chunk[0] ^ chunk[2];
+            chunk[0] = chunk[0] ^ chunk[2];
+        }
+
+        let mut png_bytes: Vec<u8> = Vec::with_capacity(nbits * 4);
+
+        if let Err(err) = image::write_buffer_with_format(
+            &mut Cursor::new(&mut png_bytes),
+            bmp_bytes,
+            bitmap.bmWidth as u32,
+            bitmap.bmHeight as u32,
+            ColorType::Rgba8,
+            image::ImageOutputFormat::Png,
+        ) {
+            return Err(core_error!(
+                "write desktop screenshot image buffer failed ({})",
+                err
+            ));
+        }
+
+        Ok(png_bytes)
+    }
+}
