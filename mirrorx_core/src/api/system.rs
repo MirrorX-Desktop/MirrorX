dced9c6b8e3b5f8c0042c8276d9b7cff2488f8c4
chenbaiyu <cby40899570@gmail.com>
2023-01-30 02:03:45 +0800

    feat(macos): hide/show remote mouse shape when move in/out desktop frame area

--- mirrorx_core/src/api/system.rs
+++ mirrorx_core/src/api/system.rs
@@ -3,4 +3,15 @@ pub fn set_show_cursor(show: bool) {
     unsafe {
         let _ = windows::Win32::UI::WindowsAndMessaging::ShowCursor::<bool>(show);
     }
+
+    #[cfg(target_os = "macos")]
+    unsafe {
+        use core_graphics::display::{CGDisplayHideCursor, CGDisplayShowCursor};
+
+        if show {
+            CGDisplayShowCursor(0);
+        } else {
+            CGDisplayHideCursor(0);
+        }
+    }
 }
